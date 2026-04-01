0b1fadd4e598b223e55ed0e1e3c2662074509e0e
Abdieeel <marcelopostigo54@outlook.com>
2026-04-01 12:18:14 -0500

    Primer commit - proyecto inicializado

---
+++ src/main/java/com/utp/sistemaincidencias/SistemaIncidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.utp.sistemaincidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaIncidenciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaIncidenciasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utp/sistemaincidencias/SistemaIncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.sistemaincidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaIncidenciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
